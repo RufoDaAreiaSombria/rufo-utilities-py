83cea2d3a49948bd6f23ab415c9e55ae9700eaad
Rufo <rufodaareiasombria@gmail.com>
2024-01-28 19:25:20 -0300

    Tesouros T20 Python
    Apanhando pra fazer só o ND1/4

--- tesouros-t20/T20.py
+++ tesouros-t20/T20.py
@@ -1,5 +1,6 @@
 from random import randint
 from time import sleep
+from dinheiro import *
 
 def RolagensIniciais():
     global rolldinheiro
@@ -10,70 +11,114 @@ def RolagensIniciais():
     print(f'Você rolou {rolldinheiro} para dinheiro e {rollitem} para item.')
 
 def ndquarto():
+    global dfinal
     RolagensIniciais()
+    dfinal = dinheirondquarto(rolldinheiro)
 
 def ndmeio():
+    global dfinal
     RolagensIniciais()
+    dfinal = dinheirondmeio(rolldinheiro)
 
 def nd1():
+    global dfinal
     RolagensIniciais()
+    dfinal = dinheirond1(rolldinheiro)
 
 def nd2():
+    global dfinal
     RolagensIniciais()
+    dfinal = dinheirond2(rolldinheiro)
 
 def nd3():
+    global dfinal
     RolagensIniciais()
+    dfinal = dinheirond3(rolldinheiro)
 
 def nd4():
+    global dfinal
     RolagensIniciais()
+    dfinal = dinheirond4(rolldinheiro)
 
 def nd5():
+    global dfinal
     RolagensIniciais()
+    dfinal = dinheirond5(rolldinheiro)
 
 def nd6():
+    global dfinal
     RolagensIniciais()
+    dfinal = dinheirond6(rolldinheiro)
 
 def nd7():
+    global dfinal
     RolagensIniciais()
+    dfinal = dinheirond7(rolldinheiro)
 
 def nd8():
+    global dfinal
     RolagensIniciais()
+    dfinal = dinheirond8(rolldinheiro)
 
 def nd9():
+    global dfinal
     RolagensIniciais()
+    dfinal = dinheirond9(rolldinheiro)
 
 def nd10():
+    global dfinal
     RolagensIniciais()
+    dfinal = dinheirond10(rolldinheiro)
 
 def nd11():
+    global dfinal
     RolagensIniciais()
+    dfinal = dinheirond11(rolldinheiro)
 
 def nd12():
+    global dfinal
     RolagensIniciais()
+    dfinal = dinheirond12(rolldinheiro)
 
 def nd13():
+    global dfinal
     RolagensIniciais()
+    dfinal = dinheirond13(rolldinheiro)
 
 def nd14():
+    global dfinal
     RolagensIniciais()
+    dfinal = dinheirond14(rolldinheiro)
 
 def nd15():
+    global dfinal
     RolagensIniciais()
+    dfinal = dinheirond15(rolldinheiro)
 
 def nd16():
+    global dfinal
     RolagensIniciais()
+    dfinal = dinheirond16(rolldinheiro)
 
 def nd17():
+    global dfinal
     RolagensIniciais()
+    dfinal = dinheirond17(rolldinheiro)
 
 def nd18():
+    global dfinal
     RolagensIniciais()
+    dfinal = dinheirond18(rolldinheiro)
 
 def nd19():
+    global dfinal
     RolagensIniciais()
+    dfinal = dinheirond19(rolldinheiro)
 
 def nd20():
+    global dfinal
     RolagensIniciais()
+    dfinal = dinheirond20(rolldinheiro)
 
 
 def definirRolagem():
@@ -134,3 +179,9 @@ while True:
         print('Por favor, digite um ND válido.')
 
 definirRolagem()
+if dfinal != 0:
+    sleep(0.4)
+    print(f'O dinheiro encontrado nesse tesouro foi {dfinal}.')
+else:
+    sleep(0.4)
+    print('Você não encontrou dinheiro nesse tesouro.')
--- tesouros-t20/dinheiro.py
+++ tesouros-t20/dinheiro.py
@@ -1,12 +1,16 @@
 from random import randint
 from time import sleep
 
-dfinal = 0
+def dinheirorep0():
+    sleep(0.4)
+    print('Sua rolagem de dinheiro não rendeu nada.')
 
 def dinheirorep1():
     d1 = randint(1, 4)
-    dfinal = d1 * 100
+    dfinal = f'{d1 * 100} T$'
+    sleep(0.4)
     print(f'O seu dinheiro rolou {d1} * 100 T$')
+    return dfinal
 
 def dinheirorep2():
     d1 = randint(1, 12)
@@ -14,13 +18,17 @@ def dinheirorep2():
     d3 = randint(1, 12)
     d4 = randint(1, 12)
     dfinal = (d1 + d2 + d3 + d4) * 10
+    sleep(0.4)
     print(f'O seu dinheiro rolou ({d1} + {d2} + {d3} + {d4}) * 10 T$')
+    return dfinal
 
 def dinheirorep3():
     d1 = randint(1, 10)
     d2 = randint(1, 10)
     dfinal = (d1 + d2) * 100
+    sleep(0.4)
     print(f'O seu dinheiro rolou ({d1} + {d2}) * 100 T$')
+    return dfinal
 
 def dinheirorep4():
     d1 = randint(1, 6)
@@ -28,223 +36,47 @@ def dinheirorep4():
     d3 = randint(1, 6)
     d4 = randint(1, 6)
     dfinal = (d1 + d2 + d3 + d4) * 100
+    sleep(0.4)
     print(f'O seu dinheiro rolou ({d1} + {d2} + {d3} + {d4}) * 100 T$')
+    return dfinal
 
 def dinheirorep5():
     d1 = randint(1, 6)
     d2 = randint(1, 6)
     d3 = randint(1, 6)
     dfinal = (d1 + d2 + d3) * 1000
+    sleep(0.4)
     print(f'O seu dinheiro rolou ({d1} + {d2} + {d3}) * 1000 T$')
+    return dfinal
 
 def dinheirorep6():
     d1 = randint(1, 4)
     d2 = randint(1, 4)
     dfinal = (d1 + d2) * 1000
+    sleep(0.4)
     print(f'O seu dinheiro rolou ({d1} + {d2}) * 1000 TO')
+    return dfinal
 
 
-def dinheirondquarto():
+def dinheirondquarto(rolldinheiro):
     if rolldinheiro < 30:
-
+        dinheirorep0()
+        return 0
     elif rolldinheiro < 70:
-
+        d1 = randint(1, 6)
+        dfinal = f'{d1 * 10} TC'
+        sleep(0.4)
+        print(f'O seu dinheiro rolou {d1} * 10 TC')
+        return dfinal
     elif rolldinheiro < 95:
-
+        d1 = randint(1, 4)
+        dfinal = f'{d1 * 100} TC'
+        sleep(0.4)
+        print(f'O seu dinheiro rolou {d1} * 100 TC')
+        return dfinal
     else:
-
-def dinheirondmeio():
-    if rolldinheiro < 25:
-
-    elif rolldinheiro < 70:
-
-    elif rolldinheiro < 95:
-
-    else:
-
-def dinheirond1():
-    if rolldinheiro < 20:
-
-    elif rolldinheiro < 70:
-
-    elif rolldinheiro < 95:
-
-    else:
-
-def dinheirond2():
-    if rolldinheiro < 15:
-
-    elif rolldinheiro <55:
-
-    elif rolldinheiro < 85:
-
-    elif rolldinheiro < 95:
-
-    else:
-
-def dinheirond3():
-    if rolldinheiro < 10:
-
-    elif rolldinheiro < 20:
-
-    elif rolldinheiro < 60:
-    
-    elif rolldinheiro < 90:
-
-    else:
-
-def dinheirond4():
-    if rolldinheiro < 10:
-
-    elif rolldinheiro < 50:
-
-    elif rolldinheiro < 80:
-
-    elif rolldinheiro < 90:
-
-    else:
-
-def dinheirond5():
-    if rolldinheiro < 15:
-
-    elif rolldinheiro < 65:
-
-    elif rolldinheiro < 95:
-
-    else:
-
-def dinheirond6():
-    if rolldinheiro < 15:
-
-    elif rolldinheiro < 60:
-
-    elif rolldinheiro < 90:
-
-    else:
-
-def dinheirond7():
-    if rolldinheiro < 10:
-
-    elif rolldinheiro < 60:
-
-    elif rolldinheiro < 90:
-
-    else:
-
-def dinheirond8():
-    if rolldinheiro < 10:
-
-    elif rolldinheiro < 55:
-
-    elif rolldinheiro < 95:
-
-    else:
-
-def dinheirond9():
-    if rolldinheiro < 10:
-
-    elif rolldinheiro < 35:
-
-    elif rolldinheiro < 85:
-
-    else:
-
-def dinheirond10():
-    if rolldinheiro < 10:
-
-    elif rolldinheiro < 30:
-
-    elif rolldinheiro < 85:
-
-    else:
-
-def dinheirond11():
-    if rolldinheiro < 10:
-
-    elif rolldinheiro < 45:
-
-    elif rolldinheiro < 85:
-
-    else:
-
-def dinheirond12():
-    if rolldinheiro < 10:
-
-    elif rolldinheiro < 45:
-
-    elif rolldinheiro < 80:
-
-    else:
-
-def dinheirond13():
-    if rolldinheiro < 10:
-
-    elif rolldinheiro < 45:
-
-    elif rolldinheiro < 80:
-
-    else:
-
-def dinheirond14():
-    if rolldinheiro < 10:
-
-    elif rolldinheiro < 45:
-
-    elif rolldinheiro < 80:
-
-    else:
-
-def dinheirond15():
-    if rolldinheiro < 10:
-
-    elif rolldinheiro < 45:
-
-    elif rolldinheiro < 80:
-
-    else:
-
-def dinheirond16():
-    if rolldinheiro < 10:
-
-    elif rolldinheiro < 40:
-
-    elif rolldinheiro < 75:
-
-    else:
-
-def dinheirond17():
-    if rolldinheiro < 5:
-
-    elif rolldinheiro < 40:
-
-    elif rolldinheiro < 75:
-
-    else:
-
-def dinheirond18():
-    if rolldinheiro < 5:
-
-    elif rolldinheiro < 40:
-
-    elif rolldinheiro < 75:
-
-    else:
-
-def dinheirond19():
-    if rolldinheiro < 5:
-
-    elif rolldinheiro < 40:
-
-    elif rolldinheiro < 75:
-
-    else:
-
-def dinheirond20():
-    if rolldinheiro < 5:
-
-    elif rolldinheiro < 40:
-
-    elif rolldinheiro < 75:
-
-    else:
-
+        d1 = randint(1, 6)
+        dfinal = f'{d1 * 10} T$'
+        sleep(0.4)
+        print(f'O seu dinheiro rolou {d1} * 10 T$')
+        return dfinal
