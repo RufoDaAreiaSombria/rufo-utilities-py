554ddc8f3fae1c017bb835abd9970bd86e50efef
Rufo <rufodaareiasombria@gmail.com>
2024-04-07 18:52:39 -0300

    Fazendo umas Listinhas
    mais listinhas rápidas de itens pra ficar pronto pra programar depois

--- tesouros-t20/item.py
+++ tesouros-t20/item.py
@@ -129,3 +129,158 @@ def esotéricos():
     (85, 'Tomo Hermético'),
     (100, 'Varinha Arcana')
 ]
+
+def apriarma():
+    apriarma = [
+    (10, 'Atroz'),
+    (13, 'Banhada a Ouro'),
+    (23, 'Certeira'),
+    (26, 'Cravejada de Gemas'),
+    (36, 'Cruel'),
+    (39, 'Discreta'),
+    (44, 'Equilibrada'),
+    (48, 'Harmonizada'),
+    (53, 'Injeção Alquímica'),
+    (55, 'Macabra'),
+    (65, 'Maciça'),
+    (75, 'Material Especial'),
+    (80, 'Mira telescópica'),
+    (90, 'Precisa'),
+    (100, 'Pungente')
+    ]
+
+def apridura():
+    apridura = [
+    (15, 'Ajustada'),
+    (19, 'Banhada a Ouro'),
+    (23, 'Cravejada de Gemas'),
+    (28, 'Delicada'),
+    (32, 'Discreta'),
+    (37, 'Espinhos'),
+    (40, 'Macabra'),
+    (50, 'Material Especial'),
+    (55, 'Polida'),
+    (80, 'Reforçada'),
+    (90, 'Selada'),
+    (100, 'Sob Medida')
+    ]
+
+def aprieso():
+    aprieso = [
+    (4, 'Banhada a ouro'),
+    (8, 'Cravejada de gemas'),
+    (12, 'Discreto'),
+    (27, 'Energético'),
+    (42, 'Harmonizado'),
+    (45, 'Macabra'),
+    (54, 'Material especial2'),
+    (70, 'Poderoso'),
+    (85, 'Potencializador'),
+    (100, 'Vigilante')
+    ]
+
+def encantoarma():
+    encantoarma = [
+    (5, 'Ameaçadora'),
+    (10, 'Anticriatura'),
+    (12, 'Arremesso'),
+    (14, 'Assassina'),
+    (16, 'Caçadora'),
+    (21, 'Congelante'),
+    (23, 'Conjuradora'),
+    (28, 'Corrosiva'),
+    (30, 'Dançarina'),
+    (34, 'Defensora'),
+    (36, 'Destruidora'),
+    (38, 'Dilacerante'),
+    (40, 'Drenante'),
+    (45, 'Elétrica'),
+    (46, 'Energética*'),
+    (48, 'Excruciante'),
+    (53, 'Flamejante'),
+    (63, 'Formidável'),
+    (64, 'Lancinante*'),
+    (72, 'Magnífica*'),
+    (74, 'Piedosa'),
+    (76, 'Profana'),
+    (78, 'Sagrada'),
+    (80, 'Sanguinária'),
+    (82, 'Trovejante'),
+    (84, 'Tumular'),
+    (88, 'Veloz'),
+    (90, 'Venenosa'),
+    (100, 'Arma Específica')
+    ]
+
+def encantodura():
+    encantodura = [
+    (6, 'Abascanto'),
+    (10, 'Abençoado'),
+    (12, 'Acrobático'),
+    (14, 'Alado'),
+    (16, 'Animado1'),
+    (18, 'Assustador'),
+    (22, 'Cáustica'),
+    (32, 'Defensor'),
+    (34, 'Escorregadio'),
+    (36, 'Esmagador1'),
+    (38, 'Fantasmagórico'),
+    (40, 'Fortificado'),
+    (44, 'Gélido'),
+    (54, 'Guardião2'),
+    (56, 'Hipnótico'),
+    (58, 'Ilusório'),
+    (62, 'Incandescente'),
+    (68, 'Invulnerável'),
+    (72, 'Opaco'),
+    (78, 'Protetor'),
+    (80, 'Refletor'),
+    (84, 'Relampejante'),
+    (86, 'Reluzente'),
+    (88, 'Sombrio'),
+    (90, 'Zeloso')
+    (100, 'Item Específico')
+    ]
+
+def armaespecifica():
+    armaespecifica = [
+    (5, 'Azagaia dos relâmpagos'),
+    (15, 'Espada baronial'),
+    (25, 'Lâmina da luz'),
+    (30, 'Lança animalesca'),
+    (35, 'Maça do terror'),
+    (40, 'Florete fugaz'),
+    (45, 'Cajado da destruição'),
+    (50, 'Cajado da vida'),
+    (55, 'Machado silvestre'),
+    (60, 'Martelo de Doherimm'),
+    (67, 'Arco do poder'),
+    (72, 'Língua do deserto'),
+    (77, 'Besta explosiva'),
+    (82, 'Punhal sszzaazita'),
+    (87, 'Espada sortuda'),
+    (92, 'Avalanche'),
+    (95, 'Cajado do poder'),
+    (100, 'Vingadora sagrada')
+    ]
+
+def itemespecifico():
+    itemespecifico = [    
+    (10, 'Cota élfica'),
+    (20, 'Couro de monstro'),
+    (25, 'Escudo do conjurador'),
+    (32, 'Loriga do centurião'),
+    (42, 'Manto da noite'),
+    (49, 'Couraça do comando'),
+    (59, 'Baluarte anão'),
+    (66, 'Escudo espinhoso'),
+    (76, 'Escudo do leão'),
+    (83, 'Carapaça demoníaca'),
+    (88, 'Escudo do eclipse'),
+    (93, 'Escudo de Azgher'),
+    (100, 'Armadura da luz')
+    ]
+
+
+
+
